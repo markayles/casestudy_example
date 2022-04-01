39c77f246261b76d5f2c5b50d0680864db845177
Mark Ayles <marka2049@gmail.com>
2022-04-01 17:04:01 -0400

    Add form bean and save new user to database

--- src/main/java/teksystems/casestudy/controller/UserController.java
+++ src/main/java/teksystems/casestudy/controller/UserController.java
@@ -1,15 +1,22 @@
 package teksystems.casestudy.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import teksystems.casestudy.database.dao.UserDAO;
+import teksystems.casestudy.database.entity.User;
+import teksystems.casestudy.formbean.RegisterFormBean;
 
 @Slf4j
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserDAO userDao;
 
     @RequestMapping(value = "/user/register", method = RequestMethod.GET)
     public ModelAndView register() throws Exception{
@@ -22,12 +29,20 @@ public class UserController {
     }
 
     @RequestMapping(value = "/user/registerSubmit", method = RequestMethod.POST)
-    public ModelAndView registerSubmit(@RequestParam("email") String email) throws Exception{
+    public ModelAndView registerSubmit(RegisterFormBean form) throws Exception{
         ModelAndView response = new ModelAndView();
-
         response.setViewName("user/register");
 
-        log.info("email from form submission: " + email);
+        User user = new User();
+
+        user.setEmail(form.getEmail());
+        user.setFirstName(form.getFirstName());
+        user.setLastName(form.getLastName());
+        user.setPassword(form.getPassword());
+
+        userDao.save(user);
+
+        log.info(form.toString());
 
         return response;
 
---
+++ src/main/java/teksystems/casestudy/formbean/RegisterFormBean.java
@@ -0,0 +1,16 @@
+package teksystems.casestudy.formbean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class RegisterFormBean {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String confirmPassword;
+}
